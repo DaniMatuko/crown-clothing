a399798cf11c2eb8b7f596ee52cf9584eb90e5ef
Dani Matuko <matuko305@gmail.com>
2020-10-30 20:36:57 +0200

    header and logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import "./App.css";
 import Homepage from "./pages/homepage/homepage";
 import { Route, Switch } from "react-router-dom";
-import Shop from "./pages/shop/shop"
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>HATS</h1>
-    </div>
-  );
-};
+import Shop from "./pages/shop/shop";
+import Header from "./components/header/header";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Homepage}></Route>
-      <Route path="/hats" component={HatsPage}></Route>
-      <Route path="/shop" component={Shop}></Route>
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage}></Route>
+        <Route path="/shop" component={Shop}></Route>
+      </Switch>
+    </div>
   );
 }
 
