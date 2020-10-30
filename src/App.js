0de53b1ca9f6a7ce4571e0e5b112f93d2c68447b
Dani Matuko <matuko305@gmail.com>
2020-10-30 19:58:09 +0200

    setting shop page and collection preview components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import "./App.css";
-import Hompeage from "./Homepage"
+import Homepage from "./pages/homepage/homepage";
+import { Route, Switch } from "react-router-dom";
+import Shop from "./pages/shop/shop"
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS</h1>
+    </div>
+  );
+};
 
 function App() {
-  return <Hompeage/>
+  return (
+    <Switch>
+      <Route exact path="/" component={Homepage}></Route>
+      <Route path="/hats" component={HatsPage}></Route>
+      <Route path="/shop" component={Shop}></Route>
+    </Switch>
+  );
 }
 
 export default App;
